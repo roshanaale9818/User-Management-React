35c70145f8cde039f283c5e9a81aa67442ee79d9
roshanaale9818 <itachiuchhia9818@gmail.com>
2023-05-13 02:23:54 +1000

    react portals added

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
+import ReactDOM from 'react-dom'
 
+const Backdrop = (props) => {
+    return (
+        <div className={classes.backdrop} onClick={props.onConfirm}></div>
 
-const ErrorModal = (props) => {
-    return <div> <div className={classes.backdrop} onClick={props.onConfirm}></div> <Card className={classes.modal}>
+    );
+}
+const Overlay = (props) => {
+    return <React.Fragment>
+        <Card className={classes.modal}>
         <header className={classes.header}>
             <h2>{props.title}</h2>
         </header>
@@ -16,6 +23,32 @@ const ErrorModal = (props) => {
             <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
     </Card>
-    </div>
+    </React.Fragment>
+}
+const ErrorModal = (props) => {
+
+    return (<React.Fragment>
+        {/* <div className={classes.backdrop} onClick={props.onConfirm}></div> 
+        <Card className={classes.modal}>
+        <header className={classes.header}>
+            <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+            <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+    </Card> */}
+        {
+            ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root'))
+        }
+        {
+            ReactDOM.createPortal(<Overlay title={props.title} message={props.message} onConfirm={props.onConfirm} />,
+                document.getElementById('overlay-root'))
+        }
+
+    </React.Fragment>)
 }
 export default ErrorModal;
